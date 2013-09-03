45f14260ae79206fc9c1f09227db4fb1ef9796c0
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-09-03 09:47:10 +0200

    slave: Use logging method of setup for logging in session module

--- src/session.py
+++ src/session.py
@@ -20,27 +20,27 @@ class Session(object):
         self.setup = setup
         
     def prepare(self, data):
-        print("[" + self.session_key + "] preparing setup")
+        self.setup.log("preparing setup")
         self.setup.load()
         self.setup.prepare_base()
         
     def add_node(self, data):
         try:
             (action, data, address, netmask) = data.split(" ", 3)
-            print("[" + self.session_key + "] add node '" + data + "'")
+            self.setup.log("add node '" + data + "'")
             self.setup.add_node(data, (address, netmask))
         except ValueError:
             pass
         
     def remove_node(self, data):
         (action, data) = data.split(" ", 1)
-        print("[" + self.session_key + "] remove node '" + data + "'")
+        self.setup.log("remove node '" + data + "'")
         self.setup.remove_node(data)
         
     def action(self, data):
         try:
             (action, data) = data.split("\n", 1)
-            print("call action: " + action)
+            self.setup.log("call action: " + action)
             ret = self.setup.action(action)
             if ret != None:
                 self.request.send(ret + "\n")
@@ -49,17 +49,17 @@ class Session(object):
         
     def run(self, data):
         """ run the nodes """
-        print("[" + self.session_key + "] run all the nodes")
+        self.setup.log("run all the nodes")
         self.setup.startup()
     
     def stop(self, data):
         """ stop the nodes """
-        print("[" + self.session_key + "] stop all the nodes")
+        self.setup.log("stop all the nodes")
         self.setup.shutdown()
     
     def cleanup(self, data):
         """ cleanup the setup """
-        print("[" + self.session_key + "] cleaning up")
+        self.setup.log("cleaning up")
         
         """ stop all nodes """
         self.setup.shutdown()
