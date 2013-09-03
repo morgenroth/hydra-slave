20386501df71ff284ff3bbbb9a196f21d6cc06e1
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-09-03 13:55:01 +0200

    slave: Use multi-dimensional array for stats data

--- src/control.py
+++ src/control.py
@@ -210,41 +210,51 @@ class NodeControl(object):
         
         try:
             """ stats interfaces """
+            stats_result["iface"] = {}
             result = self.query(("stats", "interfaces"))
             for line in result:
                 if len(line.strip()) > 0:
                     (key, data) = line.split(" ", 1)
+                    stats_result["iface"][key] = {}
                     for entry in data.split(" "):
                         (vkey, value) = entry.split(":", 1)
-                        stats_result["iface:" + key + ":" + vkey] = value.strip()
+                        stats_result["iface"][key][vkey] = value.strip()
+
+            """ dtnd stats """
+            stats_result["dtnd"] = {}
 
             """ dtnd stats info """
+            stats_result["dtnd"]["info"] = {}
+            
             result = self.query(("dtnd", "stats", "info"))
             for line in result:
                 if len(line.strip()) > 0:
                     (key, data) = line.split(":", 1)
-                    stats_result["dtnd:info:" + key] = data.strip()
+                    stats_result["dtnd"]["info"][key] = data.strip()
                     
             """ dtnd stats bundles """
+            stats_result["dtnd"]["bundles"] = {}
             result = self.query(("dtnd", "stats", "bundles"))
             for line in result:
                 if len(line.strip()) > 0:
                     (key, data) = line.split(":", 1)
-                    stats_result["dtnd:bundles:" + key] = data.strip()
+                    stats_result["dtnd"]["bundles"][key] = data.strip()
             
             """ clock get all """
+            stats_result["clock"] = {}
             result = self.query(("clock", "get", "all"))
             for line in result:
                 if len(line.strip()) > 0:
                     (key, data) = line.split(":", 1)
-                    stats_result["clock:" + key] = data.strip()
+                    stats_result["clock"][key] = data.strip()
                     
             """ get position """
+            stats_result["position"] = {}
             result = self.query(("position", "get"))
             for line in result:
                 if len(line.strip()) > 0:
                     (key, data) = line.split(":", 1)
-                    stats_result["position:" + key] = data.strip()
+                    stats_result["position"][key] = data.strip()
 
         except socket.error, msg:
             self.log("[ERROR] " + str(msg))
