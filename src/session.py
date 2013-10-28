a4e064b5229218f5c018d5a7d3461f9e86baa0a8
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-10-28 11:28:41 +0100

    Add option to configure an slave as fake

--- src/session.py
+++ src/session.py
@@ -18,6 +18,9 @@ class Session(object):
     
     """ configuration object """
     config = None
+    
+    """ if true, the session will fake all commands """
+    fake = False
 
     def __init__(self, config, session_id, hydra_url):
         '''
@@ -27,48 +30,57 @@ class Session(object):
         self.session_id = session_id
         self.hydra_url = hydra_url
         self.setup = Setup(self)
+        self.fake = (self.config.get('general', 'fake') == "yes")
         
     def prepare(self):
         self.setup.log("preparing setup")
-        self.setup.load()
-        self.setup.prepare_base()
+        if not self.fake:
+            self.setup.load()
+            self.setup.prepare_base()
         
     def add_node(self, node_id, node_name, ip_address, netmask):
         try:
             self.setup.log("add node " + str(node_id) + " '" + str(node_name) + "'")
-            self.setup.add_node(node_id, node_name, (ip_address, netmask))
+            if not self.fake:
+                self.setup.add_node(node_id, node_name, (ip_address, netmask))
         except ValueError:
             pass
         
     def remove_node(self, node_id):
         self.setup.log("remove node " + str(node_id))
-        self.setup.remove_node(node_id)
+        if not self.fake:
+            self.setup.remove_node(node_id)
         
     def action(self, data):
         try:
             self.setup.log("call action: " + data)
-            return self.setup.action(data)
+            if not self.fake:
+                return self.setup.action(data)
         except ValueError:
             pass
         
     def run(self):
         """ run the nodes """
         self.setup.log("run all the nodes")
-        self.setup.startup()
+        if not self.fake:
+            self.setup.startup()
     
     def stop(self):
         """ stop the nodes """
         self.setup.log("stop all the nodes")
-        self.setup.shutdown()
+        if not self.fake:
+            self.setup.shutdown()
     
     def cleanup(self):
         """ cleanup the setup """
         self.setup.log("cleaning up")
         
         """ stop all nodes """
-        self.setup.shutdown()
+        if not self.fake:
+            self.setup.shutdown()
         
         """ delete the setup folder """
-        self.setup.cleanup()
+        if not self.fake:
+            self.setup.cleanup()
     
     
